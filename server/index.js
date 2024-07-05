df5abfb3e4e0ea6cfedf7aaea65ca540514a324b
Tanu <developertanukaushik@gmail.com>
2024-07-05 14:49:10 +0530

    mongo configuration

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-import express from "sxpress";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv0";
+import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
-import morgan from "morgon";
+import morgan from "morgan";
 import path from "path";
 import {fileURLToPath} from "url";
-
+// import { register } from "./controllers/auth.js";
 // configuration
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app,use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
@@ -35,4 +35,20 @@ const storage = multer.diskStorage({
     }
 });
 
+// routes with files
+// app.POST("/auth/register", upload.single("picture"), register)
+
 const upload = multer({storage});
+
+// MONGOOSE  SETUP
+
+const PORT = process.env.PORT || 6001;
+const uri = process.env.MONGOURL;
+mongoose
+.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error) => console.log `${error} did not connect`)
